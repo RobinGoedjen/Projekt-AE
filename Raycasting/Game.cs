90eaa03fb4f43711884e819e3f25014aa0e87744
Robin Gödjen <mail@robingoedjen.xyz>
2020-11-02 15:05:42 +0100

    Altered gitignore Added OpenTK Created Basic Game Window

---
+++ Raycasting/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace Raycasting
+{
+    class Game : GameWindow
+    {
+        public Game() : base(500, 300, GraphicsMode.Default, "Raycasting", GameWindowFlags.Fullscreen)
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState input = Keyboard.GetState();
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+
+    }
+}

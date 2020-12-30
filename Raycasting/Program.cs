ec7789239cd83aa958457951862b0125495c9534
Robin Gödjen <mail@robingoedjen.xyz>
2020-12-30 12:34:46 +0100

    Corrected FOV Plane

--- Raycasting/Program.cs
+++ Raycasting/Program.cs
@@ -24,7 +24,7 @@ namespace Raycasting
                 return;
             }
             //22,12 old start pos 66 old FOV
-            var player = new Player(new Vector2(5, 3), new Vector2(-1, 0), new Vector2(0, 95f));
+            var player = new Player(new Vector2(5, 3), new Vector2(-1, 0), new Vector2(0, 0.95f));
             var map = JsonConvert.DeserializeObject<Map>(File.ReadAllText(mapPath));
             using (Game game = new Game(player, map))
             {
